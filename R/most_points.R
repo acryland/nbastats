ab24b18f4935a82abee182cb9d5ca69fa4276b40
Alison <acorinne13@hotmail.com>
2020-03-04 18:10:12 -0500

    Replace new files

---
+++ R/most_points.R
@@ -0,0 +1,13 @@
+#' This function returns the name and points of the highest scoring player for a given year.
+#' @param x deienfes the dataset
+#' @param y difnes the year
+#' @export
+#' @examples
+#' corr_data()
+
+most_points <- function(x, y) {
+  filter(x, Year == y) %>%
+    select(Player, PTS) %>%
+    arrange(desc(PTS)) %>%
+    slice(1)
+}
